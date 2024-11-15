56a585b3a1b65e77db4b05f476d023efff23bbbb
Mincheol-Ha <hmc0501@gmail.com>
2024-11-15 18:02:37 +0900

    branch 에서 06으로 이름 바꿈

--- springbootclass-bbs05/src/main/java/com/springbootstudy/bbs/SpringbootclassBbs06Application.java
+++ springbootclass-bbs05/src/main/java/com/springbootstudy/bbs/SpringbootclassBbs06Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootclassBbs05Application {
+public class SpringbootclassBbs06Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootclassBbs05Application.class, args);
+		SpringApplication.run(SpringbootclassBbs06Application.class, args);
 	}
 
 }
